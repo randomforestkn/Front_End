0b2932a539f3db8b07687ed73fa20210d52c97f9
randomforestkn <102574565+randomforestkn@users.noreply.github.com>
2022-03-30 20:16:25 +0300

    Add files via upload

---
+++ Glassmorphism_Hover_Effect/script.js
@@ -0,0 +1,22 @@
+let marker = document.getElementById('marker');
+let list = document.querySelectorAll('ul li');
+
+function moveIndicator(e) {
+  marker.style.left = e.offsetLeft + 'px';
+  marker.style.width = e.offsetWidth + 'px';
+}
+
+function activeLink() {
+  list.forEach((item) =>
+    item.classList.remove('active'));
+    this.classList.add('active');
+}
+
+list.forEach((item) =>
+  item.addEventListener('mouseover', activeLink));
+
+list.forEach( link => {
+  link.addEventListener('mousemove', (e) => {
+    moveIndicator(e.target);
+  })
+})
